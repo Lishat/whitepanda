054465473be30cee9c7a490811e921a989492762
Subhadeep Dash <dashsubhadeep@gmail.com>
2019-09-30 02:10:16 +0530

    Updates

---
+++ precache-manifest.3b5c42f35d063cc39ba8030b997cb103.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beae914b707d01f255dea1dbe818c66e",
+    "url": "/whitepanda/index.html"
+  },
+  {
+    "revision": "25c9f4853e9d2f9a154c",
+    "url": "/whitepanda/static/css/2.9f3a1459.chunk.css"
+  },
+  {
+    "revision": "547259d84890aeead577",
+    "url": "/whitepanda/static/css/main.67ba7fc6.chunk.css"
+  },
+  {
+    "revision": "25c9f4853e9d2f9a154c",
+    "url": "/whitepanda/static/js/2.8fc122e4.chunk.js"
+  },
+  {
+    "revision": "547259d84890aeead577",
+    "url": "/whitepanda/static/js/main.5d889a60.chunk.js"
+  },
+  {
+    "revision": "1f87031fa0def91b3c1a",
+    "url": "/whitepanda/static/js/runtime-main.c2c346a5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/whitepanda/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/whitepanda/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/whitepanda/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/whitepanda/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/whitepanda/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
